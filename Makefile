5cf206efaaaf199fbd8a99312172dae3702d7ec1
Malpp <cpt.ortan@gmail.com>
2017-11-09 12:36:14 -0500

    Added read function?

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-obj-m += test-module.o
+obj-m += usb-mouse.o
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
--- usb-mouse.c
+++ usb-mouse.c
@@ -2,7 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/usb.h>
  
-static int pen_probe(struct usb_interface *interface, const struct usb_device_id *id)
+static int mouse_probe(struct usb_interface *interface, const struct usb_device_id *id)
 {
     printk(KERN_INFO "Pen drive (%04X:%04X) plugged\n", id->idVendor, id->idProduct);
     return 0;
@@ -20,26 +20,36 @@ static struct usb_device_id mouse_table[] =
 };
 MODULE_DEVICE_TABLE (usb, mouse_table);
  
+static ssize_t skel_read( struct file *file, char *buffer, size_t count,
+	loff_t *ppos )
+{
+	printk( KERN_INFO *buffer );
+}
+
+static const struct file_operations mouse_fops = {
+	.read = skel_read
+};
+
 static struct usb_driver mouse_driver =
 {
     .name = "mouse_driver",
     .id_table = mouse_table,
-    .probe = pen_probe,
+    .probe = mouse_probe,
     .disconnect = mouse_disconnect,
 };
  
-static int __init pen_init(void)
+static int __init mouse_init(void)
 {
     return usb_register(&mouse_driver);
 }
  
-static void __exit pen_exit(void)
+static void __exit mouse_exit(void)
 {
     usb_deregister(&mouse_driver);
 }
  
-module_init(pen_init);
-module_exit(pen_exit);
+module_init(mouse_init);
+module_exit(mouse_exit);
  
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Malpp");
